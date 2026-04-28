a991903a8bb677f459f1216cf3eb779280ce434d
João Pedro <jpguinati@gmail.com>
2026-04-28 20:32:10 -0300

    feat(user): create user entity structure

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,11 +2,22 @@ package controller
 
 import (
 	"crud-api/src/configuration/rest_err"
+	"crud-api/src/controller/model/request"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Rota incorreta")
-	c.JSON(err.Code, err)
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect filds, error=%s", err))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
